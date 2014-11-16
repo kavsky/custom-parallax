3a0fdb42f254b4b8a80332605c5cc5549b754c6f
kavsky <rohanpuri.it@gmail.com>
2014-11-17 03:49:01 +0530

    files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$(function()
+{
+	var _prlxTop = $('.paralax').offset().top,
+		_prlxHt = $('.paralax').height(),
+		winHt = $(window).height(),
+		winScrl = 0,
+		_scrlIndx = 0,
+		_scrlCount,
+		_prlxY = $('.paralax').css('background-position').split(" ") ;
+
+		//var _prlxY = 
+	$(document).scroll(function()
+		{
+			winScrl = $(document).scrollTop();
+			if(( (winScrl + winHt) >= _prlxTop ) && (( _prlxTop + _prlxHt ) > winScrl ))	// within parallax condition
+			{
+				// console.log(_prlxY[1]);
+				// ptop + w
+				_scrlCount = ( _prlxTop > winHt ) ? (_prlxTop - winHt) : 0 ;
+				_scrlIndx = (( winScrl - _scrlCount ) * 0.2 ) + parseInt(_prlxY[1]) ;
+				console.log(_scrlIndx);
+				$('.paralax').css('background-position','50% '+_scrlIndx+'px');
+				
+			}
+		})
+});
